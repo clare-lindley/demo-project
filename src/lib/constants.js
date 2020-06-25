9d4d60a897c8fbe5ab9f38355ce7eea0de76cdef
Clare Lindley <clare.lindley@gmail.com>
2020-06-25 15:53:26 +0100

    Make constants more constant

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,8 +4,8 @@ const INVALID_TOKEN_MSG = "Token cannot be verified";
 const UNAUTHORISED_MSG = "Access to this resource is unauthorised.";
 const SECRET = "IAMASECRET";
 
-module.exports = {
+module.exports = Object.freeze({
 	INVALID_TOKEN_MSG: INVALID_TOKEN_MSG,
 	UNAUTHORISED_MSG: UNAUTHORISED_MSG,
 	SECRET: SECRET
-};
+});
